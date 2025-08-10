ca4e765138de5a0662d002e1a86fc543b3f4fb09
MayKama <62993571+MayKama@users.noreply.github.com>
2025-08-10 14:45:04 +0100

    Update sca.py

--- SCA_Climate/sca.py
+++ SCA_Climate/sca.py
@@ -5,8 +5,11 @@ import numpy as np
 
 def main(): 
   st.title("SCA Climate")
-
-if __name__ == "__main__":
+  num1 = st.input_number("Kindly enter your age")
+  num2 = st.input_number("Enter your height")
+  st.write(num1, num2)
   
+  
+if __name__ == "__main__":
   main()
 
